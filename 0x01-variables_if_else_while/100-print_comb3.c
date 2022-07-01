64e5e9883a0ff6cf7b724aef00ee542ee3f0f8a0
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-01 14:40:09 -0700

    Combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,15 +15,18 @@ int main(void)
 
 	for (i = '0'; i <= '8'; i++)
 	{
-		for (j = i + '1'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			putchar(i);
-			putchar(j);
-		}
-		if (i != '8' && j != '9')
-		{
-			putchar(',');
-			putchar(' ');
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i != '8' && j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
