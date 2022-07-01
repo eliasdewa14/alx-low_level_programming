7c95ea7edf6e539da60f2d256030daadc9125241
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-01 15:46:55 -0700

    Combination of three digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints three digits numbers
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if(!(i == 7 && j == 8 && k == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+			k = '0';
+		}
+		j = '0';
+	}
+	puchar('\n');
+	return (0);
+}
