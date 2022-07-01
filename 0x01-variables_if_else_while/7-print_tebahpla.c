c6e8260e190a458f6eeb4e90dbf54e1fa91f8c5d
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-01 12:57:42 -0700

    Lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - print from z to a
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	char ch = 'z';
+	
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
