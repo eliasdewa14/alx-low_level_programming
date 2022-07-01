14ecceb96baef2e11ae0c7428820b7dbb599718a
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-01 07:51:02 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print out some partsof the statment with error
+ *
+ * Return: 1 if Success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful
+- Dora Korpar, 2015-10-19\n", 58);
+	return (0);
+}
