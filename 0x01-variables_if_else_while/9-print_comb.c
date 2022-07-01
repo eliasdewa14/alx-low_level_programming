d3649b6f460b2348349c0c10bf0ec05a65a289bd
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-01 13:19:06 -0700

    Combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - print the comnibation of single digit num
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= 9)
+	{
+		putchar(i);
+		if (i != 9)
+		{
+			putchar(', ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
