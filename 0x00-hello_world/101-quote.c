f45b97b2dea5dcf75b1a751a66f10853d64a973f
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-03 08:02:05 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - print out some partsof the statment with error
  *
- * Return: 1 if Success
+ * Return: 1 always
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\"
+	write(2, "and that piece of art is useful\"
 - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
