c851cfc5f1ea11789fcc9fb9f6fd6d4de8031575
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-05 11:56:13 -0700

    9 times tabel

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,6 +14,11 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			_putchar((i * j) + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('$');
 		_putchar('\n');
