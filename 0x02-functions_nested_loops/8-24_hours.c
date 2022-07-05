4d8efd40db2312396af300d4742f38a4ee00cf85
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-05 08:26:44 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: minutes
+ */
+int jack_bauer()
+{
+	int hr = 0;
+	int min = 0;
+
+	while (hr < 24)
+	{
+		while (min < 60)
+		{
+			_putchar((hr / 10) + '0');
+		 	_putchar((hr % 10) + '0');
+			_putchar(':');
+		 	_putchar((min / 10) + '0'); 
+			_putchar((min % 10) + '0');
+			min++;
+			_putchar('\n');
+		}
+		hr++;
+	}
+}
