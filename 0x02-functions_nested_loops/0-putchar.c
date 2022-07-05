38c1e66bc25d992f89cf5a3008d4d5f156e543ef
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-05 02:06:30 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - print '_putchar'
+ *
+ * Return: 0 always Success
+ */
+int main(void)
+{
+	char ch[] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		putchar(ch[i]);
+		i++;
+	}
+	putchar('\n');
+	return 0;
+}
