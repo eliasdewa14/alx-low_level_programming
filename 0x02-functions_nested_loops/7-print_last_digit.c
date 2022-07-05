2c7642cd41e13c1410bb96728b2f073aed8afe5d
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-05 08:06:27 -0700

    The last digit of number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,13 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = n * (-1);
+		i = (n % 10) * (-1);
 	}
-	i = n % 10;
-
-	_putchar(i + '0');
-	return (i);
+	else
+	{
+		i = n % 10;
+	}
+	
+	_putchar((i % 10) + '0');
+	return (i % 10);
 }
