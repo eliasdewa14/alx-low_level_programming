366f0773889ef419c375a9777f5023b04cee181f
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-05 13:25:04 -0700

    All natural number from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural number from n to 98
+ * @n: input parameter for testing
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != n || i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar(i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != n || i != 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                        _putchar(i);
+                }
+	}
+	else
+	{
+		_putchar(n);
+	}
+}
+
