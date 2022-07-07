2d1eb6b0770ea68c49bbc05ce9d6ce475e14a9ff
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-07 13:57:59 -0700

    Traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle shape
+ * @size: input parameter for the size of triangle
+ *
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(32);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
