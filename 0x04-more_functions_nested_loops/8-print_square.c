1ab23c6e386e455310a77c99e8c50e2fe9ef4287
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-07 12:29:13 -0700

    Print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,17 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
