702f5c7c338e69d720c12b25511eab8d82d04ff6
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-11 13:20:50 -0700

    String in to integers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,20 +8,22 @@
  */
 int _atoi(char *s)
 {
-	unsigned int n;
-	int i = 0, neg = 1, br;
+	unsigned int n = 0;
+	int i = 0, neg = 1;
+	char br = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			neg *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			n *= 10;
-			n += (s[i] + '0');
+			n += (s[i] - '0');
 			br = 1;
 		}
-		else if (br == 1)
+		else if (br)
 			break;
 		i++;
 	}
