a5f78dde4b3c1bb5ac7ce69831891b47fad7448b
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-11 07:29:05 -0700

    Reverse the strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print the strings in reverse
+ * @s: input pointer characters
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (i = len; i > 0; i--)
+		_putchar(s[i]);
+}
