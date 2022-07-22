0bb13aa6b6a492c15398c3348662f3c4b9aaf739
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-22 14:02:33 -0700

    Static libraries

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strstr - print that locates a substring
+ * @haystack: input string
+ * @needle: input string locates haystack string
+ *
+ * Return: located substring
+ */
+
