23a93c4a6a75a9b5e229cedad6304faeafa83ebb
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-26 06:45:56 -0700

    Debugging

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 	if (str == NULL || str == 0)
-		return (0);
+		return (NULL);
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
