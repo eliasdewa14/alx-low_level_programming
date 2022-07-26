b22911211ce30c9776d3948935dae1cfda18700d
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-26 12:47:21 -0700

    Debugging 100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,7 +26,7 @@ char *argstostr(int ac, char **av)
 		len++;
 		i++;
 	}
-	str = malloc(sizeof(*str) * (len + 1));
+	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 	m = 0;
