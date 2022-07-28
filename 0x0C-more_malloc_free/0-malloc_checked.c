4d8bc42537cd6ecb6bea7d1c3ea346d96008b14f
Elias-Dewa <elias.dewa19@gmail.com>
2022-07-28 02:52:46 -0700

    C - More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: input unsigned integer
+ *
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
