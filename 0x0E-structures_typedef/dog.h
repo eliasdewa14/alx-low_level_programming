01538c158e772e8271aea5a4b12f18e390a5be2e
Elias-Dewa <elias.dewa19@gmail.com>
2022-08-01 12:13:26 -0700

    Debug Structures and typedef tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,19 @@
  * @age: age of dog
  * @owner: Owner of the dog
  *
- */ 
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
