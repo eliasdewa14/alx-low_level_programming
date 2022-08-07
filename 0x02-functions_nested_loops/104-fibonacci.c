ffbf4e039644e0d3d89ac9ee91761c54e1d759a4
Elias-Dewa <elias.dewa19@gmail.com>
2022-08-07 13:47:31 -0700

    Debug fix for 104-fibonacci task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,7 +16,7 @@ int main(void)
 	while (i <= 49)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
-		if (fib[i] % 2 == 0 && fib[i] < 4000000)
+		if ((fib[i] % 2) == 0 && fib[i] < 4000000)
 			sum += fib[i];
 		i++;
 	}
