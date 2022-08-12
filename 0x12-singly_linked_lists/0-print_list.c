2af5354cc60d594d52a676dcce6b3d5050db95d2
Elias-Dewa <elias.dewa19@gmail.com>
2022-08-12 13:28:25 -0700

    Bug fixed for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list - print a linkd list
+ * print_list - print  a linkd list
  * @h: pointer to head of list
  *
  * Return: number of nodes
@@ -13,14 +13,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str)
-		{
+		if (h->str != NULL)
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		else
-		{
 			printf("[0] (nil)\n");
-		}
 		h = h->next;
 		n++;
 	}
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
+#include <stdio.h>
 #include <string.h>
-unsigned int _strlen(const char *str);
+#include <stdlib.h>
 
 /**
  * add_node_end - a function that adds a new node at the end of a list
@@ -11,40 +12,40 @@ unsigned int _strlen(const char *str);
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
-	
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+        list_t *new_node, *temp;
 
-	new_node->len = _strlen(str);
-	new_node->str = strdup(str);
-	if (*head == NULL)
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else
-	{
-		new_node->next = NULL;
-		temp = *head;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new_node;
-	}
-	return (new_node);
+        new_node = malloc(sizeof(list_t));
+        if (new_node == NULL)
+                return (NULL);
+
+        new_node->len = str_len(str);
+        new_node->str = strdup(str);
+        if (*head == NULL)
+        {
+                new_node->next = *head;
+                *head = new_node;
+        }
+        else
+        {
+                new_node->next = NULL;
+                temp = *head;
+                while (temp->next)
+                        temp = temp->next;
+                temp->next = new_node;
+        }
+        return (new_node);
 }
 
 /**
- * _strlen - return the length of string
+ * str_len - return the length of string
  * @str: pointer to string
  *
  * Return: string length
  */
-unsigned int _strlen(const char *str)
+unsigned int str_len(const char *str)
 {
-	unsigned int length = 0;
-	while (str[length])
-		length++;
-	return (length);
+        unsigned int length = 0;
+        while (str[length])
+                length++;
+        return (length);
 }
