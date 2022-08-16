cc757df095b3677b781790e4642ec76f1f928dda
Elias-Dewa <elias.dewa19@gmail.com>
2022-08-16 14:00:23 -0700

    Bugs are fixed for task 9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-	listint_t *new_node, temp;
+	listint_t *new_node, *temp;
 
 	if (head == NULL)
 		return (NULL);
