47636c66560a61235e2f30abd8a2d50a7cbd2402
Elias-Dewa <elias.dewa19@gmail.com>
2022-08-18 14:21:26 -0700

    Debug for task 2, 3, and 4

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int num = 1 << index;
-	if (num == 0)
-		return (-1);
-	if ((n & num))
-		return (1);
-	else
-		return (0);
+	unsigned int num = n >> index;
+	
+	if (index <= (sizeof(n) * 8 - 1))
+		return (num & 1);
+	return (-1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,8 +11,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int num = 1 << index;
 
-	if (num == 0)
-		return (-1);
-	*n |= num;
-	return (1);
+	if (index <= (sizeof(n) * 8 - 1))
+	{
+		*n |= num;
+		return (1);
+	}
+	return (-1);
 }
