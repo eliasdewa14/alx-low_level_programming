1e51d95f45af2be48ea01942e5da7a5dcb4ab5e3
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-29 11:09:43 -0700

    Debugged for task 2 and 6

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,7 @@
 /**
  * add_dnodeint - function that adds a new node at the beginning of a list
  * @head: pointer to list head
+ * @n: value to be added
  *
  * Return: the address of the new element, or NULL if it failed
  */
@@ -11,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * sum_dlistint -  function that returns the sum of all the data of a list
  * @head: pointer to head list
- * 
+ *
  * Return: sum, otherwise 0 if empty
  */
 int sum_dlistint(dlistint_t *head)
