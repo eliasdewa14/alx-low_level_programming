c43460ac112a9d36a9e7981a503ae8cb1e4d7945
Elias Dewa Ahmed <54449260+Elias-Dewa@users.noreply.github.com>
2022-12-07 11:01:59 -0800

    Makefiles: Bugs are fixed

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """a function to calculate the island perimeter
+
+    Args:
+        grid (list): a list of list of integer
+    Return:
+        the perimeter of the island described in grid
+    """
